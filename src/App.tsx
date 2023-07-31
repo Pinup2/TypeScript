6e34d8eb256135bc4531d5555b2f06dc4438df1b
A Dangling Hiena <anghelina@MacBook-Air.local>
2023-07-31 13:26:59 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import LoginPage from './components/LoginPage.tsx';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+    return (
+        <Router>
+        <LoginPage/>
+        </Router>
+
+    )
+
+}
+
+export default App
